60b6cd116310f6774ef2e30a3449704458cda6f3
sepehrfazeli <sky.f1118@gmail.com>
2025-09-22 14:55:04 +0200

    simple send message integrated
    Add SignalR and CORS support for React frontend

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using ChatApplication.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddSignalR();
+
+// Add CORS for React frontend
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:5173") // frontend port
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+app.UseRouting();
+
+// Map SignalR Hub
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
