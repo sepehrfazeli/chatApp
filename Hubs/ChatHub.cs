e1f20ac3218e858d9f156c493d2f43e72c258ff1
sepehrfazeli <sky.f1118@gmail.com>
2025-09-22 15:57:00 +0200

    add rename feature

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,10 +4,15 @@ namespace ChatApplication.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string userSessionId)
         {
             var timestamp = DateTime.Now.ToString("HH:mm");
-            await Clients.All.SendAsync("ReceiveMessage", user, message, timestamp);
+            await Clients.All.SendAsync("ReceiveMessage", user, message, timestamp, userSessionId);
+        }
+
+        public async Task UpdateUserName(string newName, string userSessionId)
+        {
+            await Clients.Others.SendAsync("UserNameUpdated", newName, userSessionId);
         }
     }
 }
